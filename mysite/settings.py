3fec3299d0a9f7fa3706d0be23dc881272a080b8
Aaron Rothstein <aaroths@gmail.com>
2017-11-18 12:03:51 -0600

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hqe0gqrbe7sjx0w09u1v)0_4c^k^rg-fk)o06%s#v#dau20rnn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','aaroths.pythonanywhere.com']
 
 
 # Application definition
